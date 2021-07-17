c7ea4300722345ab9838310e8b15c15efea8ea1b
Jeff Schmidt <jeff94040@gmail.com>
2021-07-17 16:05:15 -0700

    /webhook-notifications references db instead of log

--- routes.js
+++ routes.js
@@ -10,6 +10,10 @@ const router = express.Router();
 const webhookSchema = new mongoose.Schema({any: {}});
 const Webhook = mongoose.model('Webhook', webhookSchema);
 
+const mongo_db_name = process.env.MONGO_DB_NAME;
+const mongo_db_user = process.env.MONGO_DB_USER;
+const mongo_db_password = process.env.MONGO_DB_PASSWORD;
+
 // Render CKO Test Harness homepage
 router.get('/', (req, res) => {
   res.render('index');
@@ -39,7 +43,7 @@ router.post('/webhook-listener', (req, res) => {
   console.log('received webhook...');
   console.log(req.body);
 
-  mongoose.connect('mongodb+srv://jeff:Fw1KpnkhzNvzSqnk@cluster0.3gcos.mongodb.net/cko_test_harness?authSource=admin&replicaSet=atlas-h15pmt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+  mongoose.connect(`mongodb+srv://${mongo_db_user}:${mongo_db_password}@cluster0.3gcos.mongodb.net/${mongo_db_name}?authSource=admin&replicaSet=atlas-h15pmt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
@@ -63,15 +67,45 @@ router.post('/webhook-listener', (req, res) => {
 // Return webhook notifications
 router.get('/webhook-notifications', (req, res) => {
   
-  const events = fs.readFileSync(webhookNotifications, {encoding: 'utf-8'});
+  // const events = fs.readFileSync(webhookNotifications, {encoding: 'utf-8'});
+  mongoose.connect(`mongodb+srv://${mongo_db_user}:${mongo_db_password}@cluster0.3gcos.mongodb.net/${mongo_db_name}?authSource=admin&replicaSet=atlas-h15pmt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
-  res.render('webhook-notifications', {events: events});
+  const db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', function() {
+    console.log('we are connected!!');
+    // const webhookSchema = new mongoose.Schema({any: {}});
+
+    // kittySchema.methods.speak = function () {
+    //   const greeting = this.name ? "Meow name is " + this.name : "I don't have a name";
+    //   console.log(greeting);
+    // };
+
+    // const Webhook = mongoose.model('Webhook', webhookSchema);
+
+    //const webhook = new Webhook({any: {"id":"evt_g7dferlwdghudjxjhpjer6rixe","type":"payment_paid","created_on":"2021-07-01T20:50:53Z","data":{"action_id":"act_pjfe2dn7eraejhsvtc7u7tm76u","auth_code":"323163","response_code":"10000","response_summary":"Approved","amount":1000,"metadata":{},"destination":{"id":"src_z4dbhwyq5v2uznycg7tveuvp3m","type":"card","expiry_month":12,"expiry_year":2030,"name":"John Doe","scheme":"Visa","last_4":"4242","fingerprint":"0b8cdac052322fc6061aa358aeb0bd65436c95b62e1808d05940b294c902fb81","bin":"424242","card_type":"Credit","card_category":"Consumer","issuer":"JPMORGAN CHASE BANK NA","issuer_country":"US","product_id":"A","product_type":"Visa Traditional","avs_check":"S","cvv_check":"Y"},"customer":{"id":"cus_gmag4eabtcpufndrwpnaiibphu"},"id":"pay_iitlf7c4vdxmfjircd4q4vauty","currency":"EUR","processed_on":"2021-07-01T20:51:00Z"},"_links":{"self":{"href":"https://api.sandbox.checkout.com/events/evt_g7dferlwdghudjxjhpjer6rixe"},"payment":{"href":"https://api.sandbox.checkout.com/payments/pay_iitlf7c4vdxmfjircd4q4vauty"}}}});
+/*
+    webhook.save(function (err, webhook) {
+      if(err) return console.error(err);
+      console.log('wrote webhook to database!');
+    });
+*/
+    Webhook.find(function (err, webhooks) {
+      if (err) return console.error(err);
+      console.log(webhooks);
+      res.status(200).json(webhooks);
+    });
+
+  });
+
 
 });
 
 router.get('/mongodb', (req, res) => {
 /*
-  mongoose.connect('mongodb+srv://jeff:Fw1KpnkhzNvzSqnk@cluster0.3gcos.mongodb.net/cko_test_harness?authSource=admin&replicaSet=atlas-h15pmt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+  mongoose.connect(`mongodb+srv://${mongo_db_user}:${mongo_db_password}@cluster0.3gcos.mongodb.net/${mongo_db_name}?authSource=admin&replicaSet=atlas-h15pmt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
