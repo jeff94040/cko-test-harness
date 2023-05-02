a7a2751aec7b7f030cedc9f9308ee26484f84994
Jeff Schmidt <jeff94040@gmail.com>
2023-05-02 15:09:56 -0700

    added Apple Pay implementation

---
+++ public/apple-pay.js
@@ -0,0 +1,60 @@
+// determine level of device, browser, and wallet support for Apple Pay and display the appropriate results
+if (!window.ApplePaySession) {
+  document.querySelector('#apple-pay-availability').innerHTML = 'This browser does not support Apple Pay. Try Safari on any device or Chrome on iOS.'
+}
+else if(!ApplePaySession.canMakePayments){
+  document.querySelector('#apple-pay-availability').innerHTML = 'This browser supports Apple Pay, but the device is not capable of making Apple Pay payments.'
+}
+else{
+  const applePayMerchantID = await (await fetch('/apple-pay-merchant-id')).text()
+
+  const activeCard = await ApplePaySession.canMakePaymentsWithActiveCard(applePayMerchantID);
+
+  if(activeCard)
+    document.querySelector('#apple-pay-button').removeAttribute('style')
+  else
+    document.querySelector('#apple-pay-availability').innerHTML = 'This device and browser both support Apple Pay. Either your wallet does not have an eligible payment method or wallet access is prevented by use of an external monitor.'
+}
+
+// when user clicks the apple pay button, we must create/begin an apple pay session, validate the merchant, and submit a payment
+document.querySelector('apple-pay-button').addEventListener('click', () => {
+  // create session
+  var applePaySession = new ApplePaySession(14, {
+    merchantCapabilities: ['supports3DS'],
+    supportedNetworks: ['visa', 'masterCard', 'amex', 'discover'],
+    countryCode: 'US',
+    total: {label: "Jeff's Test Account", amount: '3.00'},
+    lineItems: [{label: 'Widget A', amount: '1.00'}, {label: 'Widget B', amount: '2.00'}],
+    currencyCode: 'USD',
+  })
+  // begin session
+  applePaySession.begin()
+
+  // validate merchant
+  applePaySession.onvalidatemerchant = async (event) => {
+    const validateSessionResponse = await (await fetch('/apple-pay-validate-session', {
+      method: 'POST',
+      body: JSON.stringify({
+        validationURL: event.validationURL
+      }),
+      headers: {'Content-Type': 'application/json'}
+    })).json()
+    applePaySession.completeMerchantValidation(validateSessionResponse)
+  }
+
+  // payment authorized by user
+  applePaySession.onpaymentauthorized = async (event) => {
+    // submit payment
+    const paymentResponse = await fetch('/apple-pay-payment', {
+      method: 'POST',
+      body: JSON.stringify({
+        token: event.payment.token
+      }),
+      headers: {'Content-Type': 'application/json'}
+    })
+
+    // update session w/ payment results
+    applePaySession.completePayment(paymentResponse.status === 201 ? ApplePaySession.STATUS_SUCCESS : ApplePaySession.STATUS_FAILURE)
+  }
+
+})
--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import fetch from 'node-fetch'; 
+import https from 'node:https';
 import mongoose from 'mongoose';
 import crypto from 'crypto';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import { create } from 'node:domain';
 
 //import config props from .env file
 dotenv.config();
@@ -35,23 +38,124 @@ db.on('error', console.error.bind(console, 'connection error:'));
 // fires on successful database connection
 db.once('open', function() { console.log(`Connected to mongodb+srv://${mongo_db_user}:*****@${mongo_db_cluster_domain}/${mongo_db_name}`) });
 
-// Render CKO Test Harness homepage
+/***** page rendering routes *****/
+
+// render index
 router.get('/', (req, res) => {
   res.render('index');
 });
 
-// Render Frames page
+// render frames
 router.get('/frames', (req, res) => {
   res.render('frames');
 });
 
-// Get public key for Frames
+// render events
+router.get('/events', (req, res) => {
+  res.render('events');
+});
+
+// render pan generator
+router.get('/pan-generator', (req, res) => {
+  res.render('pan-generator');
+});
+
+// render plaid-ach
+router.get('/plaid-ach', (req, res) => {
+  res.render('plaid-ach');
+});
+
+// render risk.js
+router.get('/risk-js', (req, res) => {
+  res.render('risk-js');
+});
+
+// render paypal
+router.get('/paypal', (req, res) => {
+  res.render('paypal')
+})
+
+// render apple pay
+router.get('/apple-pay', (req, res) => {
+  res.render('apple-pay');
+});
+
+/***** return text values *****/
+
+// return public key
 router.get('/frames-key', (req, res) => {
-  //res.status(200).json({key: process.env.CKO_NAS_PUBLIC_KEY});
   res.send(process.env.CKO_NAS_PUBLIC_KEY)
 });
 
-// Event notification listener
+// apple pay - return merchant id
+router.get('/apple-pay-merchant-id', (req, res) => {
+  res.send(process.env.APPLE_PAY_MERCHANT_ID)
+})
+
+/***** functional post routes *****/
+
+// apple pay - create custom agent and validate session
+router.post('/apple-pay-validate-session', async (req, res) => {
+
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+    cert: fs.readFileSync(process.env.APPLE_PAY_CERTIFICATE),
+    key: fs.readFileSync(process.env.APPLE_PAY_KEY)
+  })
+
+  const validateSessionResponse = await (await fetch(req.body.validationURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      merchantIdentifier: process.env.APPLE_PAY_MERCHANT_ID,
+      domainName: process.env.APPLE_PAY_DOMAIN,
+      displayName: process.env.APPLE_PAY_DISPLAY_NAME
+    }),
+    headers: {'Content-Type': 'application/json'},
+    agent: httpsAgent
+  })).json()
+
+  res.status(200).json(validateSessionResponse);
+})
+
+// apple pay - request payment
+router.post('/apple-pay-payment', async (req, res) => {
+
+  // apple pay - create cko token
+  const createTokenResponse = await (await fetch('https://api.sandbox.checkout.com/tokens', {
+    method: 'POST',
+    body: JSON.stringify({
+      'type': 'applepay',
+      'token_data': req.body.token.paymentData
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${process.env.CKO_NAS_PUBLIC_KEY}`
+    }
+  })).json()
+
+  // apple pay - submit payment
+  const paymentResponse = await fetch('https://api.sandbox.checkout.com/payments', {
+    method: 'POST',
+    body: JSON.stringify({
+      'source': {
+        'type': 'token',
+        'token': createTokenResponse.token
+      },
+      'amount': 300,
+      'currency': 'USD',
+      'reference': `REF-${gen_ref(6)}`,
+      'processing_channel_id': process.env.CKO_NAS_PROCESSING_CHANNEL_ID
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${process.env.CKO_NAS_SECRET_KEY}`
+    }
+  })
+
+  res.status(paymentResponse.status).end()
+})
+
+// webhook listener
 router.post('/event-listener/:accountStructure', (req, res) => {
 
   console.log(`received ${req.params.accountStructure} event notification...`);
@@ -94,25 +198,10 @@ router.post('/event-listener/:accountStructure', (req, res) => {
 
 });
 
-router.get('/events', (req, res) => {
-  res.render('events');
-});
-
-router.get('/keys', (req, res) => {
-  res.render('keys');
-});
-
-router.get('/pan-generator', (req, res) => {
-  res.render('pan-generator');
-});
-
-router.get('/plaid-ach', (req, res) => {
-  res.render('plaid-ach');
-});
-
+// paypal - create order
 router.get('/paypal-create-order', async (req, res) => {
   
-  const cko_res_http = await fetch('https://api.sandbox.checkout.com/payments', {
+  const createOrderResponse = await (await fetch('https://api.sandbox.checkout.com/payments', {
     method: 'POST',
     body: JSON.stringify({
       source: {
@@ -140,104 +229,86 @@ router.get('/paypal-create-order', async (req, res) => {
       'Content-Type': 'application/json',
       'Authorization': `${process.env.CKO_NAS_SECRET_KEY}`
     }
-  });
+  })).json()
 
-  const cko_res_body = await cko_res_http.json();
-  //console.log(`response from /payments: ${JSON.stringify(cko_res_body)}`);  
-
-  res.send(cko_res_body.processing.order_id)
+  res.send(createOrderResponse.processing.order_id)
 })
 
-router.get('/paypal', (req, res) => {
-  res.render('paypal')
-})
+// plaid - create link token
+router.get('/plaid/link-token', async (req, res) => {
 
-router.get('/risk-js', (req, res) => {
-  res.render('risk-js');
+  const tokenCreateResponse = await (await fetch('https://sandbox.plaid.com/link/token/create', {
+    method: 'POST',
+    body: JSON.stringify({
+      client_id: process.env.PLAID_CLIENT_ID,
+      secret: process.env.PLAID_SANDBOX_KEY,
+      user: {
+        client_user_id: "client_user_id_1"
+      },
+      client_name: "Jeff's CKO Test Harness",
+      products: ["auth"],
+      country_codes: ["US"],
+      language: "en"
+    }),
+    headers: {'Content-Type': 'application/json'}
+  })).json()
+  console.log(`response from /link/token/create: ${JSON.stringify(tokenCreateResponse)}`);
+  res.status(200).json(tokenCreateResponse);
+
 });
 
-router.get('/plaid/link-token', (req, res) => {
-
-    fetch('https://sandbox.plaid.com/link/token/create', {
-	    method: 'POST',
-	    body: JSON.stringify({
-        client_id: process.env.PLAID_CLIENT_ID,
-        secret: process.env.PLAID_SANDBOX_KEY,
-        user: {
-          client_user_id: "client_user_id_1"
-        },
-        client_name: "Jeff's CKO Test Harness",
-        products: ["auth"],
-        country_codes: ["US"],
-        language: "en"
-      }),
-	    headers: {'Content-Type': 'application/json'}
-    })
-    .then(response => response.json())
-    .then(body => {
-      console.log(`response from /link/token/create: ${JSON.stringify(body)}`);
-      res.status(200).json(body);
-    })
-    
-});
-
+// plaid - token exchange and payment
 router.post('/plaid/access-token', async (req, res) => {
 
-  const public_res_http = await fetch('https://sandbox.plaid.com/item/public_token/exchange', {
+  const exchangeResponse = await (await fetch('https://sandbox.plaid.com/item/public_token/exchange', {
     method: 'POST',
     body: JSON.stringify({
       client_id: process.env.PLAID_CLIENT_ID,
       secret: process.env.PLAID_SANDBOX_KEY,
       public_token: req.body.public_token}),
     headers: {'Content-Type': 'application/json'}
-  });
+  })).json()
+  console.log(`response from /item/public_token/exchange: ${JSON.stringify(exchangeResponse)}`);
 
-  const public_res_body = await public_res_http.json();
-  console.log(`response from /item/public_token/exchange: ${JSON.stringify(public_res_body)}`);
-
-  const processor_res_http = await fetch('https://sandbox.plaid.com/processor/token/create', {
+  const processorTokenResponse = await (await fetch('https://sandbox.plaid.com/processor/token/create', {
     method: 'POST',
     body: JSON.stringify({
       client_id: process.env.PLAID_CLIENT_ID,
       secret: process.env.PLAID_SANDBOX_KEY,
-      access_token: public_res_body.access_token,
+      access_token: exchangeResponse.access_token,
       account_id: req.body.account_id,
       processor: 'checkout'}),
     headers: {'Content-Type': 'application/json'}
-  });
+  })).json()
+  console.log(`response from /processor/token/create: ${JSON.stringify(processorTokenResponse)}`);
 
-  const processor_res_body = await processor_res_http.json();
-  console.log(`response from /processor/token/create: ${JSON.stringify(processor_res_body)}`);
-
-  const cko_res_http = await fetch('https://api.sandbox.checkout.com/payments', {
+  const paymentResponse = await (await fetch('https://api.sandbox.checkout.com/payments', {
     method: 'POST',
     body: JSON.stringify({
       source: {
           type: 'provider_token',
           payment_method: 'ach',
-          token: processor_res_body.processor_token,
+          token: processorTokenResponse.processor_token,
           account_holder: {
               type: 'Individual'
           }
       },
       amount: 4500,
       currency: 'USD',
-      processing_channel_id: 'pc_i7u3hlih2nze7mq6digv3pemuq'
+      reference: `REF-${gen_ref(6)}`,
+      processing_channel_id: process.env.CKO_NAS_PROCESSING_CHANNEL_ID
     }),
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `${process.env.CKO_NAS_SECRET_KEY}`
     }
-  });
+  })).json()
+  console.log(`response from /payments: ${JSON.stringify(paymentResponse)}`);
 
-  const cko_res_body = await cko_res_http.json();
-  console.log(`response from /payments: ${JSON.stringify(cko_res_body)}`);
-
-  res.status(200).json(cko_res_body);
-
+  res.status(200).json(paymentResponse);
 });
 
-// Return events
+// webhooks - return all events
 router.post('/fetch-events', (req, res) => {
 
   // console.log('request received @ /fetch-events');
@@ -255,10 +326,13 @@ router.post('/fetch-events', (req, res) => {
 router.post('/fetch-api-request', async (req, res) => {
 
   // default key to sk, reassign to pk for /tokens endpoint only
-  let key = process.env.CKO_ABC_SECRET_KEY;
+  let key = process.env.CKO_NAS_SECRET_KEY;
   if(req.body.path === '/tokens')
-    key = process.env.CKO_ABC_PUBLIC_KEY;
+    key = process.env.CKO_NAS_PUBLIC_KEY;
 
+  // add processing channel to all requests
+  req.body.body['processing_channel_id'] = process.env.CKO_NAS_PROCESSING_CHANNEL_ID
+  
   const data = {
     method: req.body.verb,
     headers: {
@@ -287,5 +361,14 @@ router.post('/fetch-api-request', async (req, res) => {
     res.send({status: 'exception', body: error});
   }
 });
+
+// helper function to generate a random reference
+function gen_ref(length){
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for ( let i = 0; i < length; i++ )
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  return result;
+}
 
 export {router};
