c3f5020f7fabb56df46a2e01dc055be058118427
Jeff Schmidt <jeff94040@gmail.com>
2023-05-23 17:19:10 -0700

    sift key removal

--- public/js/sift.js
+++ public/js/sift.js
@@ -1,16 +1,10 @@
-const siftBeaconKey = '69cbcb9c6d' //sandbox
-const siftUserId = '632ca582511045537ef2a951' //sandbox
-
-// const siftBeaconKey = 'e298acbdc4' //production
-// const siftUserId = '632ca582511045537ef2a94e' //production
+const siftBeaconKey = await (await fetch('/sift/beacon-key')).text()
+const siftUserId = await (await fetch('/sift/user-id')).text()
 
 const cookie = document.cookie
 const start = cookie.indexOf('=')
 const end = cookie.indexOf(';')
 const sessionId = cookie.substring(start+1, end)
-
-console.log(`sessionId: ${sessionId}`)
-console.log('got here')
 
 var _user_id = siftUserId; // Set to the user's ID, username, or email address, or '' if not yet known.
 var _session_id = sessionId; // Set to a unique session ID for the visitor's current browsing session.
--- routes/sift.js
+++ routes/sift.js
@@ -2,10 +2,13 @@ import express from 'express';
 
 const siftRouter = express.Router();
 
-siftRouter.get('/sift-session-id', async (req, res) => {
-
-  //console.log(req)
-  res.status(200)
+siftRouter.get('/sift/:property', (req, res) => {
+  if (req.params.property === 'beacon-key')
+    res.send(process.env.SIFT_BEACON_KEY)
+  else if (req.params.property === 'user-id')
+    res.send(process.env.SIFT_USER_ID)
+  else
+    res.status(400)
 })
 
 export {siftRouter}; 
