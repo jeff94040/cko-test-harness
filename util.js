8e892ebcd9503cb8fdf8e0c291af6e701e458811
Jeff Schmidt <jeff94040@gmail.com>
2024-05-28 14:37:54 -0700

    a file was deleted

--- util.js
+++
@@ -1,8 +0,0 @@
-// helper function to generate a random reference
-export function generateReference(length){
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let result = 'REF-';
-  for ( let i = 0; i < length; i++ )
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  return result;
-}
